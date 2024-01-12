1b481bd5b1fcee0643e33853afc368b5c35004cd
Donato Souza <donatosouza33@gmail.com>
2024-01-12 15:28:54 -0300

    Fazendo o botão play funcionar.

--- game_functions.py
+++ game_functions.py
@@ -21,7 +21,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """Responde a eventos de pressionamento de teclas e de mouse."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,6 +30,10 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
+
 def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Atualiza as imagens na tela e alterna para a nova tela."""
     #Redesenha a tela a cada passagem pelo laço
@@ -154,3 +158,8 @@ def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
             # Trata esse caso do mesmo modo que é feito quando a espaçonave é atingida
             ship_hit(ai_settings, stats, screen,ship, aliens, bullets)
             break
+
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Inicia um novo jogo quando o jogador clicar em Play."""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
