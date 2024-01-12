934bbe0f0ade6e60cf5e5e388cd1ea0e6eb223f4
Donato Souza <donatosouza33@gmail.com>
2024-01-12 16:00:30 -0300

    Criando uma classe para exibir a pontuação.

--- game_stats.py
+++ game_stats.py
@@ -10,3 +10,4 @@ class GameStats():
     def reset_stats(self):
         """Inicializa os dados estatísticos que podem mudar durante o jogo."""
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
